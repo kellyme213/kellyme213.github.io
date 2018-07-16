9aa3936968c14de119c03bf9f08e69b57d885e7a
kellyme213 <kellyme213@gmail.com>
2018-07-15 23:23:05 -0700

    7/15

--- scripts/startup.js
+++ scripts/startup.js
@@ -1,6 +1,9 @@
-console.log(navigator.userAgent);
+//console.log(navigator.userAgent);
 
-if ((navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1) || navigator.userAgent.indexOf("Edge") != -1)
+var isSafari = (navigator.userAgent.indexOf("Safari") != -1 && navigator.userAgent.indexOf("Chrome") == -1);
+var isEdge = navigator.userAgent.indexOf("Edge") != -1;
+
+if (isSafari || isEdge)
 {
     var style = document.createElement("style");
     style.innerHTML = ".projectBox, .textbox {background-color: rgba(0, 0, 0, 0);}\
