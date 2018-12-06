6af2a979e9734347c3ebc2c9da8f8748b261a89d
kellyme213 <kellyme213@gmail.com>
2018-12-05 23:36:10 -0500

    12/5

--- CG/homeworks/finalProject/plasmaBall.js
+++ CG/homeworks/finalProject/plasmaBall.js
@@ -1,7 +1,8 @@
 
 var gl;
 var canvas;
-var program;
+var tendrilProgram;
+var ballProgram;
 var points;
 var tendrils = [];
 
@@ -29,7 +30,22 @@ var blendAnglePointer;
 var tendrilColorBlendBufferID;
 var tendrilColorBlendPointer;
 
+var rotationAngleXLocation;
+var rotationAngleYLocation;
+var tendrilColorLocation;
+
 var numTendrilPoints = 0;
+var rotationAngleX = 0.0;
+var rotationAngleY = 0.0;
+
+var toggleBlend = true;
+var toggleUpdate = true;
+
+var numTendrils = 100;
+var tendrilR = 0.4;
+var tendrilG = 0.4;
+var tendrilB = 1.0;
+var sphereVertices = [];
 
 window.onload = function init()
 {
@@ -37,15 +53,89 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    for (var x = 0; x < 100; x++)
+    for (var x = 0; x < numTendrils; x++)
     {
         tendrils.push(new Tendril());
     }
+
     
     initializeData();
     updateBlendValues();
+    
+    
+    document.addEventListener('keydown', inputFunction, false);
+    document.getElementById("r-slider").oninput = function(event)
+    {
+        tendrilR = event.target.value;
+        gl.uniform3fv(tendrilColorLocation, flatten([tendrilR, tendrilG, tendrilB]));
+    }
+    
+    document.getElementById("g-slider").oninput = function(event)
+    {
+        tendrilG = event.target.value;
+        gl.uniform3fv(tendrilColorLocation, flatten([tendrilR, tendrilG, tendrilB]));
+    }
+    
+    document.getElementById("b-slider").oninput = function(event)
+    {
+        tendrilB = event.target.value;
+        gl.uniform3fv(tendrilColorLocation, flatten([tendrilR, tendrilG, tendrilB]));
+    }
+    
     updateLoop();
+    
+    
 };
+
+function inputFunction(event)
+{
+    //window.alert(event.keyCode);
+    var key = event.keyCode;
+    
+    if (key == 37)
+    {
+        rotationAngleX = (rotationAngleX + 0.05) % 6.28;
+        gl.uniform1f(rotationAngleXLocation, rotationAngleX);
+    }
+    if (key == 39)
+    {
+        rotationAngleX = (rotationAngleX - 0.05) % 6.28;
+        gl.uniform1f(rotationAngleXLocation, rotationAngleX);
+    }
+    if (key == 38)
+    {
+        rotationAngleY = (rotationAngleY + 0.05) % 6.28;
+        gl.uniform1f(rotationAngleYLocation, rotationAngleY);
+    }
+    if (key == 40)
+    {
+        rotationAngleY = (rotationAngleY - 0.05) % 6.28;
+        gl.uniform1f(rotationAngleYLocation, rotationAngleY);
+    }
+    if (key == 32)
+    {
+        rotationAngleY = 0.0;
+        rotationAngleX = 0.0;
+        gl.uniform1f(rotationAngleXLocation, rotationAngleX);
+        gl.uniform1f(rotationAngleYLocation, rotationAngleY);
+    }
+    if (key == 49)
+    {
+        toggleBlend = !toggleBlend;
+    }
+    if (key == 50)
+    {
+        toggleUpdate = !toggleUpdate;
+    }
+    
+    
+    document.getElementById("xAngleP").innerHTML = "X angle - " + rotationAngleX;
+    document.getElementById("yAngleP").innerHTML = "Y angle - " + rotationAngleY;
+    document.getElementById("numTendrilP").innerHTML = "Number of Tendrils - " + numTendrils;
+    document.getElementById("updateP").innerHTML = "Tendril update - " + toggleUpdate;
+    document.getElementById("blendP").innerHTML = "Blend update - " + toggleBlend;
+
+}
 
 function initializeData()
 {
@@ -94,6 +184,9 @@ function initialTendrilBufferLoad()
     
     putDataInBuffer(startDirectionBufferID, startDirections, startDirectionPointer, 3);
     putDataInBuffer(endDirectionBufferID, endDirections, endDirectionPointer, 3);
+    
+    gl.uniform3fv(tendrilColorLocation, flatten([tendrilR, tendrilG, tendrilB]));
+
 }
 
 function initializeGLParameters()
@@ -101,29 +194,33 @@ function initializeGLParameters()
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     
-    program = initShaders(gl, "vertex-shader", "fragment-shader");
-    gl.useProgram(program);
+    tendrilProgram = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(tendrilProgram);
     
     startVertexBufferID = gl.createBuffer();
-    startVertexPointer = gl.getAttribLocation(program, "startV");
+    startVertexPointer = gl.getAttribLocation(tendrilProgram, "startV");
     
     endVertexBufferID = gl.createBuffer();
-    endVertexPointer = gl.getAttribLocation(program, "endV");
+    endVertexPointer = gl.getAttribLocation(tendrilProgram, "endV");
     
     blendVertexBufferID = gl.createBuffer();
-    blendVertexPointer = gl.getAttribLocation(program, "vertexBlendVal");
+    blendVertexPointer = gl.getAttribLocation(tendrilProgram, "vertexBlendVal");
     
     startDirectionBufferID = gl.createBuffer();
-    startDirectionPointer = gl.getAttribLocation(program, "startDirection");
+    startDirectionPointer = gl.getAttribLocation(tendrilProgram, "startDirection");
     
     endDirectionBufferID = gl.createBuffer();
-    endDirectionPointer = gl.getAttribLocation(program, "endDirection");
+    endDirectionPointer = gl.getAttribLocation(tendrilProgram, "endDirection");
     
     blendAngleBufferID = gl.createBuffer();
-    blendAnglePointer = gl.getAttribLocation(program, "angleBlendVal");
+    blendAnglePointer = gl.getAttribLocation(tendrilProgram, "angleBlendVal");
     
     tendrilColorBlendBufferID = gl.createBuffer();
-    tendrilColorBlendPointer = gl.getAttribLocation(program, "tendrilColorBlend");
+    tendrilColorBlendPointer = gl.getAttribLocation(tendrilProgram, "tendrilColorBlend");
+    
+    rotationAngleXLocation = gl.getUniformLocation(tendrilProgram, "rotationAngleX");
+    rotationAngleYLocation = gl.getUniformLocation(tendrilProgram, "rotationAngleY");
+    tendrilColorLocation = gl.getUniformLocation(tendrilProgram, "tendrilCol");
     
     for (var x = 0; x < tendrils.length; x++)
     {
@@ -200,8 +297,14 @@ function updateOldTendrils()
 
 function updateLoop()
 {
-    updateOldTendrils();
-    updateBlendValues();
+    if (toggleUpdate)
+    {
+        updateOldTendrils();
+    }
+    if (toggleBlend)
+    {
+        updateBlendValues();
+    }
     renderTendrils();
     requestAnimationFrame(updateLoop);
 }
@@ -238,6 +341,26 @@ function putSubDataInBuffer(bufferID, data, bufferPointer, length, offset)
     gl.bufferSubData(gl.ARRAY_BUFFER, offset, flatten(data));
     gl.vertexAttribPointer(bufferPointer, length, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(bufferPointer);
+}
+
+
+//based on an algorithm you showed us in class.
+//used to approximate a sphere via subdividing a pyramid.
+function subdivide(p1, p2, p3, level)
+{
+    if (level <= 0)
+    {
+        sphereVertices.push(p1);
+        sphereVertices.push(p2);
+        sphereVertices.push(p3);
+        return;
+    }
+    var p4 = [(p1[0] + p2[0] + p3[0]) / 3.0,
+               (p1[1] + p2[1] + p3[1]) / 3.0,
+               (p1[2] + p2[2] + p3[2]) / 3.0];
+    subdivide(p1, p4, p3, level - 1.0);
+    subdivide(p1, p2, p4, level - 1.0);
+    subdivide(p4, p2, p3, level - 1.0);
 }
 
 //0, z, -y
