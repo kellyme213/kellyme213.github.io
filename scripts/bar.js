fececa5d60b8b94239f8b9355fbdec133f80a3d2
kellyme213 <kellyme213@gmail.com>
2018-12-27 20:58:35 -0500

    12/27

---
+++ scripts/bar.js
@@ -0,0 +1,29 @@
+generateA("Resume", "resume.html", "resumeBar");
+generateA("Course Work", "coursework.html", "courseworkBar");
+generateA("GitHub", "https://www.github.com/kellyme213", "githubBar");
+generateP("3D Art");
+generateA("3D Modeling", "modeling.html", "modelingBar");
+generateP("Graphics Projects");
+generateA("Ray Tracer", "raytracer.html", "raytracerBar");
+generateA("Plasma Ball", "plasmaball.html", "plasmaballBar");
+generateA("PyMEL Cherry Blossoms", "blossom.html", "blossomBar");
+generateP("Games");
+generateA("OXIO", "oxio.html", "oxioBar");
+generateA("Connect the Shapes!", "shapes.html", "shapesBar");
+generateA("The Dungeon", "dungeon.html", "dungeonBar");
+
+function generateA(name, link, id)
+{
+    var a = document.createElement("A");
+    a.appendChild(document.createTextNode(name));
+    a.id = id;
+    a.href = link;
+    document.getElementById("sidebar").appendChild(a);
+}
+
+function generateP(name)
+{
+    var p = document.createElement("P");
+    p.appendChild(document.createTextNode(name));
+    document.getElementById("sidebar").appendChild(p);
+}
