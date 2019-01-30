5e79843bcc95b48018840b9dce603078df34047f
Michael <kellyme213@gmail.com>
2019-01-30 14:37:41 -0500

    1/30

--- scripts/bar.js
+++ scripts/bar.js
@@ -1,10 +1,11 @@
 generateA("Resume", "resume.html", "resumeBar");
 generateA("Course Work", "coursework.html", "courseworkBar");
 generateA("GitHub", "https://www.github.com/kellyme213", "githubBar");
+generateA("Short Films", "film.html", "filmBar");
 generateP("3D Art");
 generateA("3D Modeling", "modeling.html", "modelingBar");
 generateA("Lighting", "lighting.html", "lightingBar");
-generateP("Graphics Projects");
+generateP("Graphics");
 generateA("Ray Tracer", "raytracer.html", "raytracerBar");
 generateA("Plasma Ball", "plasmaball.html", "plasmaballBar");
 generateA("PyMEL Cherry Blossoms", "blossom.html", "blossomBar");
@@ -12,9 +13,9 @@ generateP("Games");
 generateA("OXIO", "oxio.html", "oxioBar");
 generateA("Connect the Shapes!", "shapes.html", "shapesBar");
 //generateA("The Dungeon", "dungeon.html", "dungeonBar");
-generateP("Short Films");
-generateA("Films", "film.html", "filmBar");
-generateImage();
+//generateP("Short Films");
+//generateA("Films", "film.html", "filmBar");
+//generateImage();
 
 function generateA(name, link, id)
 {
