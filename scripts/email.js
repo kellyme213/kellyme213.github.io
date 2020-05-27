c3147d4d14406382d7c2a7abdb9516c5437a3f1f
Michael <kellyme213@gmail.com>
2020-05-27 15:56:33 -0400

    5/27

--- scripts/email.js
+++ scripts/email.js
@@ -1,2 +1,2 @@
 var email = "kellyme213" + "@" + "gmail.com";
-document.getElementById("headertext").innerHTML += " Feel free to contact me at " + email + "!";
+document.getElementById("headertext").innerHTML += " Want to get in touch? My email is " + email + "!";
