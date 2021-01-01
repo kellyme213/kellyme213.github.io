0349041f7380d021ea7e7bb0bd786bcd7e187f8e
Michael <kellyme213@gmail.com>
2020-12-31 23:45:40 -0500

    12/31

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,7 +1,8 @@
+generateA("Blog", "/blog/index.html");
 generateA("About", "/about.html");
-generateA("GitHub", "https://github.com/kellyme213/");
 generateA("Resume", "/resume.html");
 //generateA("LinkedIn", "https://www.linkedin.com/in/kellyme213/");
+//generateA("GitHub", "https://github.com/kellyme213/");
 
 
 function generateA(name, link)
